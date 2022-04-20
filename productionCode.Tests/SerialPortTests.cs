aaaa31fd09bf2d654c06241632aca56ea141d4fa
Maria Fernanda Del Milagro Guevara Lizarraga <mguevarali@intercorp.com.pe>
2022-04-20 15:04:45 -0500

    Agregar archivos de proyecto.

---
+++ productionCode.Tests/SerialPortTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace productionCode.Tests
+{
+    [TestFixture]
+    public class SerialPortTests
+    {
+        [Test]
+        public void serialPortCOM1DebeRetornar1()
+        {
+            //Arrange
+            SerialPortParser serialPortParser = new SerialPortParser();
+            //Act
+            int result = serialPortParser.parsePort("COM1");
+            //assert
+            //Assert.IsTrue((result ==1));//
+            Assert.That(result, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void serialPortCOM2DebeRetornar2()
+        {
+            //Arrange
+            SerialPortParser serialPortParser = new SerialPortParser();
+            //Act
+            int result = serialPortParser.parsePort("COM2");
+            //assert
+            //Assert.IsTrue((result == 2));//
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void serialPortDebeLanzarFormatExecption()
+        {
+            //Arrange y //Act
+            TestDelegate throwException = () => new SerialPortParser().parsePort("COM2#");
+            //Assert
+            var ex = Assert.Throws<FormatException>(throwException);
+            Assert.That(ex.Message, Is.EqualTo("El puerto ingresado no tiene el fomrato correcto"));
+        }
+    }
+}
---
+++ productionCode/SerialPortParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace productionCode
+{
+    public class SerialPortParser
+    {
+        public int parsePort(String port)
+        {
+            if (!new Regex(@"^(COM)[0-9]{1}$").IsMatch(port))
+            //if (!port.StartsWith("COM"))
+            {
+                throw new FormatException("El puerto ingresado no tiene el fomrato correcto");
+            }
+            else
+            {
+                return int.Parse(port.Substring(3));
+            }
+        }
+    }
+}
